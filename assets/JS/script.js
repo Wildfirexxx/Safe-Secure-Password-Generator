9638602ceef36aff1860a97f9cb217a53acbe042
Wildfirexxx <alderick@aldericks-mbp.lan>
2022-03-17 23:44:28 -0400

    added arrays to hold uppercase, lowercase, numbers, and symbols

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,17 @@
 // Assignment code here
+lowercaseArr  = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z"];
+ console.log(lowercaseArr);
 
+ uppercaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+ console.log(uppercaseArr);
 
+ numbersArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  console.log(numbersArr)
+
+  symbolsArr = ["!", "@", "#","$", "%", "^", "&", "*",];
+  console.log(symbolsArr);
+
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +26,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
