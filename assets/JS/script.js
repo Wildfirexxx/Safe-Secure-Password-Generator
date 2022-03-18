6b91a97bd345f2abcab17c2f18e7e01f57914c05
Wildfirexxx <alderick@aldericks-mbp.lan>
2022-03-18 01:12:53 -0400

    added 1 array with nested objects containing all characters

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,17 +1,36 @@
 // Assignment code here
-lowercaseArr  = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z"];
- console.log(lowercaseArr);
+var characters = [
+  {
+    characterType: "lowercase",
+      characters:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  },
+  {
+    characterType: "upperCase",
+    characters: [
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+    ]
+  },
+  {
+    characterType: "numbers",
+    characters: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  },
+  {
+    characterType: "symbols",
+    characters: ["!", "@", "#", "$", "%", "^", "&", "*", ]
+  },
+];
 
- uppercaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
- console.log(uppercaseArr);
+console.log(characters[0]);
+console.log(characters[1]);
+console.log(characters[2]);
+console.log(characters[3]);
 
- numbersArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  console.log(numbersArr)
+console.log(characters[0].characters[4]);
+console.log(characters[1].characters[4]);
+console.log(characters[2].characters[4]);
+console.log(characters[3].characters[4]);
 
-  symbolsArr = ["!", "@", "#","$", "%", "^", "&", "*",];
-  console.log(symbolsArr);
 
-  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -26,4 +45,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
